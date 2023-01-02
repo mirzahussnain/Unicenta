ebc2c692e901d9926f0ab289c68a314c20695477
Ittyab Ur Rehman <ittyaboffical@gmail.com>
2023-01-02 12:22:56 +0500

    urdu language support implementaion started leddger module added

--- src/main/java/com/diy/accounting/gui/LedgerManagement.java
+++ src/main/java/com/diy/accounting/gui/LedgerManagement.java
@@ -4,19 +4,31 @@
  */
 package com.diy.accounting.gui;
 
+import com.openbravo.basic.BasicException;
+import com.openbravo.pos.forms.AppLocal;
+import com.openbravo.pos.forms.AppView;
+import com.openbravo.pos.forms.BeanFactory;
+import com.openbravo.pos.forms.JPanelView;
+import javax.swing.JComponent;
+
 /**
  *
  * @author Ittyab Ur Rehman
  */
-public class LedgerManagement extends javax.swing.JPanel {
+public class LedgerManagement extends javax.swing.JPanel implements JPanelView, BeanFactory {
 
     /**
      * Creates new form LedgerManagement
+     * @param appView
      */
-    public LedgerManagement() {
+    public LedgerManagement(AppView appView) {
         initComponents();
     }
 
+//    public LedgerManagement() {
+//                initComponents();
+//
+//    }
     /**
      * This method is called from within the constructor to initialize the form.
      * WARNING: Do NOT modify this code. The content of this method is always
@@ -37,6 +49,30 @@ public class LedgerManagement extends javax.swing.JPanel {
             .addGap(0, 300, Short.MAX_VALUE)
         );
     }// </editor-fold>//GEN-END:initComponents
+
+    @Override
+    public String getTitle() {
+        return AppLocal.getIntString("Menu.LedgerManager");
+    }
+
+    @Override
+    public void activate() throws BasicException {
+    }
+
+    @Override
+    public boolean deactivate() {
+        return false;
+    }
+
+    @Override
+    public JComponent getComponent() {
+        return this;
+    }
+
+    @Override
+    public Object getBean() {
+        return this;
+    }
 
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
